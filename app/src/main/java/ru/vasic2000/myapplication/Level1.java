556521fac48cc67f8c554b2b0d50db692a361b86
Vasic2000 <vasic2000@yandex.ru>
2020-04-11 14:48:12 +0300

    Level 1

--- app/src/main/java/ru/vasic2000/myapplication/Level1.java
+++ app/src/main/java/ru/vasic2000/myapplication/Level1.java
@@ -2,9 +2,11 @@ package ru.vasic2000.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -13,7 +15,20 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
 
+        imgRoundCorners();
+
         FullScreen();
+    }
+
+    private void imgRoundCorners() {
+        final ImageView imgLeft = findViewById(R.id.imgLeft);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            imgLeft.setClipToOutline(true);
+        }
+        final ImageView imgRight = findViewById(R.id.imgRight);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            imgRight.setClipToOutline(true);
+        }
     }
 
     private void FullScreen() {
